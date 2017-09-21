d0c614db28329c2eb1bc5ed3d1d41e1d0e290fe3
Helgef <HelgeffegleH@users.noreply.github.com>
2017-09-21 23:00:12 +0200

    adding "regular" for loop. (getFromHash.c, next.c) adding "UTF-16" to strget in ahk_v1 version, might support ANSI.

---
+++ c source/getFromHash.c
@@ -0,0 +1,16 @@
+﻿#include "hash.h"
+node* __cdecl getFromHash(node** nodes, unsigned int hash, unsigned int n){
+	// returns the n:th node at nodes[hash], 0 if no node is found.
+	// called by next for "regular" ahk for loop. _newenum()
+	node* foundNode = nodes[hash];
+	if (foundNode == 0 || n==1)
+		return foundNode;
+	int i=1; 
+	while(foundNode->next != 0 && i<n) {
+		foundNode=foundNode->next;
+		i++;
+	}
+	if (i==n)
+		return foundNode;
+	return 0;
+}
---
+++ c source/next.c
@@ -0,0 +1,21 @@
+﻿#include "hash.h"
+typedef node* __cdecl (*getFromHash)(node**, unsigned int, unsigned int);
+//									(node** nodes, unsigned int hash, unsigned int n)
+// This if for the ahk for loop. _newenum()
+node* __cdecl next(tableData** table, unsigned int* hash, unsigned int* n, getFromHash pgetFromHash){
+	// n is the n:th key/val pair to get from bucket hash. If not exist, set n to 1 and increment hash. Else, increment n and return foundNode.
+	node* foundNode=0;
+	hash-=1;	//	due to numput returning the "wrong" address. Doing it in compiled version is ugly but improves performance. Mind the input.
+	n-=1;		//	-- "" --
+	while (*hash<(*table)->length){
+		foundNode = pgetFromHash((*table)->nodes,*hash,*n);
+		if (foundNode == 0){
+			*n=1;
+			(*hash)++;
+		} else {
+			(*n)++;
+			return foundNode;
+		}
+	}
+	return 0;
+}
